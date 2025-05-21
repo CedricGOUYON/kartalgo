5d59a67ea4e0914aa07a8041a9f44b71dd776052
Belovitchy <belo38690@gmail.com>
2025-05-21 17:52:49 +0200

    Co-authored-by: Mohamed-am10 <Mohamed-am10@users.noreply.github.com> Co-authored-by: Cédric GOUYON <gouyoncedric@gmail.com>

---
+++ src/components/animations/Animations.tsx
@@ -0,0 +1,9 @@
+import "./Animations.css";
+function Animations() {
+	return (
+		<>
+			<h1> Animations </h1>
+		</>
+	);
+}
+export default Animations;
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import App from "./App";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Game from "./Pages/Game/Game";
+import NotFound from "./Pages/NotFound/NotFound";
+
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [],
-  },
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <NotFound />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "login", element: <Login /> },
+			{ path: "game", element: <Game /> },
+			{ path: "*", element: <NotFound /> },
+		],
+	},
 ]);
 export default router;
