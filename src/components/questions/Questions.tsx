2f2566c7f513de78f47ce9a35087d14964fba807
Belovitchy <belo38690@gmail.com>
2025-05-22 14:31:33 +0200

    startAnimeKart

--- src/components/questions/Questions.tsx
+++ src/components/questions/Questions.tsx
@@ -3,32 +3,37 @@ import "./Questions.css";
 import { useGame } from "../../Context/GameContext";
 
 function Questions() {
-  const { currentQuestion, setCurrentQuestion, allQuestions, score, setScore } = useGame();
+	const { currentQuestion, setCurrentQuestion, allQuestions, score, setScore } =
+		useGame();
 
-  function handleClick(option: string, id: number) {
-    if (option === currentQuestion.answer) {
-      setCurrentQuestion(allQuestions[id++]);
-      const newScore = score + 1;
-      setScore(newScore);
-    }
-  }
+	function handleClick(option: string, id: number) {
+		if (option === currentQuestion.answer) {
+			setCurrentQuestion(allQuestions[id++]);
+			const newScore = score + 1;
+			setScore(newScore);
+		}
+	}
 
-  return (
-    <div className="question-container">
-      <h2>{currentQuestion.question}</h2>
+	return (
+		<div className="question-container">
+			<h2>{currentQuestion.question}</h2>
 
-      <ul>
-        {currentQuestion.options.map((option) => (
-          <button onClick={() => handleClick(option, currentQuestion.id)} type="button" key={option}>
-            {option}
-          </button>
-        ))}
-      </ul>
-      <section>
-        <article>{score}</article>
-        <article>Timer</article>
-      </section>
-    </div>
-  );
+			<ul>
+				{currentQuestion.options.map((option) => (
+					<button
+						onClick={() => handleClick(option, currentQuestion.id)}
+						type="button"
+						key={option}
+					>
+						{option}
+					</button>
+				))}
+			</ul>
+			<section>
+				<article className="score">{score}</article>
+				<article className="timer">Timer</article>
+			</section>
+		</div>
+	);
 }
 export default Questions;
