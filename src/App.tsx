f22cf6a1f208b9cbc8eecc49fa23ad006255a5e0
anne durand <annedurand0709@gmail.com>
2025-05-23 10:10:16 +0200

    create button about

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 import { GameProvider } from "./Context/GameContext";
+import './App.css';
 
 function App() {
   return (
+    <>
     <GameProvider>
       <Outlet />
     </GameProvider>
+    <Link to="/About">
+    <button className="about" type="button">About</button>
+</Link>
+    </>
   );
 }
 
